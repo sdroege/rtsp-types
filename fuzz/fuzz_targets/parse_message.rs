fe5203c3ed8d7b1f98ed4bf6fe68b7a319764826
Taruntej Kanakamalla <tarun@centricular.com>
2026-04-22 16:12:17 +0530

    fuzz: serialize the parsed message

--- fuzz/fuzz_targets/parse_message.rs
+++ fuzz/fuzz_targets/parse_message.rs
@@ -2,5 +2,20 @@
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    let _unused_result = rtsp_types::Message::<&[u8]>::parse(data);
+    let msg = rtsp_types::Message::<&[u8]>::parse(data);
+
+    if let Ok(m) = msg {
+        let mut data = Vec::new();
+        match m.0 {
+            rtsp_types::Message::Request(req) => {
+                let _ = req.write(&mut data);
+            }
+            rtsp_types::Message::Response(resp) => {
+                let _ = resp.write(&mut data);
+            }
+            rtsp_types::Message::Data(d) => {
+                let _ = d.write(&mut data);
+            }
+        }
+    };
 });
